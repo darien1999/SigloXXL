13c669668231bae65248b9c6f3f670c15c20c125
ns74train <benjaminalejandro60@gmail.com>
2022-10-26 05:43:17 -0300

    Subida de imagen de productos y Validacion del formulario

--- projectDjango123/icard_react/src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
+++ projectDjango123/icard_react/src/components/Admin/Product/AddEditProductForm/AddEditProductForm.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Form, Image, Button, Dropdown, Checkbox } from "semantic-ui-react";
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  Form,
+  Image,
+  Button,
+  Dropdown,
+  Checkbox,
+  Input,
+} from "semantic-ui-react";
 import { map } from "lodash";
+import { useDropzone } from "react-dropzone";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { useCategory } from "../../../../hoooks";
 import "./AddEditProductForm.scss";
 
 export function AddEditProductForm() {
   const [categoriesFormat, setCategoriesFormat] = useState([]);
+  const [previewImage, setPreviewImage] = useState(null);
   const { categories, getCategories } = useCategory();
 
   useEffect(() => getCategories(), []);
@@ -13,25 +24,73 @@ export function AddEditProductForm() {
     setCategoriesFormat(formatDropdownData(categories));
   }, [categories]);
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(newSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {},
+  });
+
+  const onDrop = useCallback(async (acceptedFile) => {
+    const file = acceptedFile[0];
+    await formik.setFieldValue("image", file);
+    setPreviewImage(URL.createObjectURL(file));
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
   return (
-    <Form className="add-edit-product-form">
-      <Form.Input name="title" placeholder="Nombre del producto" />
-      <Form.Input type="number" name="price" placeholder="Precio" />
+    <Form className="add-edit-product-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="title"
+        placeholder="Nombre del producto"
+        value={formik.values.title}
+        onChange={formik.handleChange}
+        error={formik.errors.title}
+      />
+      <Form.Input
+        type="number"
+        name="price"
+        placeholder="Precio"
+        value={formik.values.price}
+        onChange={formik.handleChange}
+        error={formik.errors.price}
+      />
       <Dropdown
         placeholder="Categoria"
         fluid
         selection
         search
         options={categoriesFormat}
+        value={formik.values.category}
+        error={formik.errors.price}
+        onChange={(_, data) => formik.setFieldValue("category", data.value)}
       />
 
       <div className="add-edit-product-form__active">
-        <Checkbox toggle />
+        <Checkbox
+          toggle
+          checked={formik.values.active}
+          onChange={(_, data) => formik.setFieldValue("active", data.checked)}
+        />
         Producto activo
       </div>
-      <Button type="button" fluid>
-        Subir Imagen
+
+      <Button
+        type="button"
+        fluid
+        {...getRootProps()}
+        color={formik.errors.image && "red"}
+      >
+        {previewImage ? "Cambiar imagen" : "Subir imagen"}
       </Button>
+      <input {...getInputProps()} />
+      <Image src={previewImage} />
 
       <Button type="submit" primary fluid content="Crear" />
     </Form>
@@ -44,4 +103,24 @@ function formatDropdownData(data) {
     text: item.title,
     value: item.id,
   }));
+}
+
+function initialValues(data) {
+  return {
+    title: "",
+    price: "",
+    category: "",
+    active: false,
+    image: "",
+  };
+}
+
+function newSchema() {
+  return {
+    title: Yup.string().required(true),
+    price: Yup.number().required(true),
+    category: Yup.number().required(true),
+    active: Yup.boolean().required(true),
+    image: Yup.string().required(true),
+  };
 }
