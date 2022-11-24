1d633469248917af7ad131d6e49f8196964e56ac
darien1999 <zhxdaso@gmail.com>
2022-11-23 21:07:02 -0300

    Creacion de Codigo QR

--- projectDjango123/icard_react/src/pages/Client/SelectTable/SelectTable.js
+++ projectDjango123/icard_react/src/pages/Client/SelectTable/SelectTable.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
-import { useTable } from "../../../hoooks"
+import { useTable } from "../../../hoooks";
 import "./SelectTable.scss";
 export function SelectTable(props) {
-  const {history} = props;
+  const { history } = props;
   const [tableNum, setTableNum] = useState(null);
   const [error, setError] = useState(null);
-  const {isExistTable} = useTable();
+  const { isExistTable } = useTable();
 
-  const onSubmit = async() => {
+  const onSubmit = async () => {
     setError(null);
     if (!tableNum) {
       setError("No has introducido ninguna mesa");
     } else {
-      const exist = await isExistTable(tableNum)
-      if(exist) history.push(`/client/${tableNum}`);
-      else setError("El numero de la mesa no existe")
-      
+      const exist = await isExistTable(tableNum);
+      if (exist) history.push(`/client/${tableNum}`);
+      else setError("El numero de la mesa no existe");
     }
   };
   //   console.log(tableNum);
   return (
     <div className="select-table">
       <div className="select-table__content">
-        <h1>Bienvenido a ICard</h1>
+        <h1>Bienvenido a SigloXXL</h1>
         <h2>Introduce tu numero de mesa</h2>
 
         <Form onSubmit={onSubmit}>
